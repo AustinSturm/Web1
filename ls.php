e98839fa3ba8439cef7488c3a3b0d8ee0c1da6ed
AustinSturm <austinsturm@knights.ucf.edu>
2016-03-25 11:00:13 -0400

    Added files via upload

---
+++ ls.php
@@ -0,0 +1,21 @@
+<html>
+<head><title></title></head>
+<body>
+
+
+
+    <form action ="" method="get">
+      <p>What files do you need?</p>
+      <input type="text" name="cmd" id="cmd" />
+      <label>
+      Submit
+      <input type="submit" name="submit" id="submit" value="Submit" />
+      </label>
+    </form>
+
+    <?php
+      $query = "ls" + $_GET['cmd'];
+      $results = exec($query);
+     ?>
+</body>
+</html>
