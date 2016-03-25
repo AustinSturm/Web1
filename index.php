f3fcdfaa8419425684763e75c21edfde1c04544d
AustinSturm <austinsturm@knights.ucf.edu>
2016-03-24 22:07:13 -0400

    Added files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  if(!isset($_COOKIE['user'])){
+    setcookie("user", $_GET['user'], -1);
+  }
+ ?>
+
+<html>
+<head><title></title></head>
+<body>
+<?php
+  $key = 'deadbeef';
+
+ ?>
+
+</body>
+</html>
